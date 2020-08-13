e28c6e4d38b68c0466db564462562add05d22cc4
mvidal90 <63307424+mvidal90@users.noreply.github.com>
2020-08-13 11:48:54 -0300

    upload customize

---
+++ customize.js
@@ -0,0 +1,60 @@
+
+function red(){
+    document.getElementById("path2427").style.fill = "red";
+    document.getElementById("path3199").style.fill = "red";
+    document.getElementById("path3203").style.fill = "red";
+    document.getElementById("path3205").style.fill = "red";
+    document.getElementById("path3209").style.fill = "red";
+    document.getElementById("path3213").style.fill = "red";
+    document.getElementById("path3215").style.fill = "red";
+    document.getElementById("path3217").style.fill = "red";
+
+    document.getElementById("path3274").style.fill = "red";
+    document.getElementById("path3276").style.fill = "red";
+    document.getElementById("path3278").style.fill = "red";
+    document.getElementById("path3284").style.fill = "red";
+    document.getElementById("path3286").style.fill = "red";
+    document.getElementById("path3288").style.fill = "red";
+    document.getElementById("path3290").style.fill = "red";
+
+}
+
+
+function green(){
+    document.getElementById("path2427").style.fill = "green";
+    document.getElementById("path3199").style.fill = "green";
+    document.getElementById("path3203").style.fill = "green";
+    document.getElementById("path3205").style.fill = "green";
+    document.getElementById("path3209").style.fill = "green";
+    document.getElementById("path3213").style.fill = "green";
+    document.getElementById("path3215").style.fill = "green";
+    document.getElementById("path3217").style.fill = "green";
+
+    document.getElementById("path3274").style.fill = "green";
+    document.getElementById("path3276").style.fill = "green";
+    document.getElementById("path3278").style.fill = "green";
+    document.getElementById("path3284").style.fill = "green";
+    document.getElementById("path3286").style.fill = "green";
+    document.getElementById("path3288").style.fill = "green";
+    document.getElementById("path3290").style.fill = "green";
+
+}
+
+function blue(){
+    document.getElementById("path2427").style.fill = "blue";
+    document.getElementById("path3199").style.fill = "blue";
+    document.getElementById("path3203").style.fill = "blue";
+    document.getElementById("path3205").style.fill = "blue";
+    document.getElementById("path3209").style.fill = "blue";
+    document.getElementById("path3213").style.fill = "blue";
+    document.getElementById("path3215").style.fill = "blue";
+    document.getElementById("path3217").style.fill = "blue";
+
+    document.getElementById("path3274").style.fill = "blue";
+    document.getElementById("path3276").style.fill = "blue";
+    document.getElementById("path3278").style.fill = "blue";
+    document.getElementById("path3284").style.fill = "blue";
+    document.getElementById("path3286").style.fill = "blue";
+    document.getElementById("path3288").style.fill = "blue";
+    document.getElementById("path3290").style.fill = "blue";
+}
